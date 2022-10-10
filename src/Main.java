84da57f7e7bc7156fb7bd9556d3a2f9c6f27ed83
iShantaram <1agapovav987@gmail.com>
2022-10-10 13:32:21 +0300

    3 tasks completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //
+        // Первое задание - инициализация массивов
+        System.out.println("Первое задание:");
         int[] arrayThreeNum = new int[3];
         arrayThreeNum[0] = 1;
         arrayThreeNum[1] = 2;
@@ -10,10 +11,15 @@ public class Main {
 
         String[] words = {"ordered", "not", "is", "array", "This"};
 
+        System.out.println("Массивы инициализированы успешно.");
+
+        // Второе задание - последовательный вывод элементов массивов
+        System.out.println("Второе задание:");
+
         for (int i = 0; i < arrayThreeNum.length; i++) {
             System.out.print(arrayThreeNum[i]);
             if (i < arrayThreeNum.length-1) {
-                System.out.print(",");
+                System.out.print(", ");
             }
         }
         System.out.println();
@@ -21,13 +27,42 @@ public class Main {
         for (int i = 0; i < arrayFloat.length; i++) {
             System.out.print(arrayFloat[i]);
             if (i < arrayThreeNum.length - 1) {
-                System.out.print(",");
+                System.out.print(", ");
             }
         }
         System.out.println();
 
         for (int i = 0; i < words.length; i++) {
-            System.out.print(words[i] + ",");
+            System.out.print(words[i]);
+            if (i < words.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        // Третье задание - обратный вывод элементов массивов
+        System.out.println("Третье задание:");
+        for (int i = 0; i < arrayThreeNum.length; i++) {
+            System.out.print(arrayThreeNum[arrayThreeNum.length-1-i]);
+            if (i < arrayThreeNum.length-1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < arrayFloat.length; i++) {
+            System.out.print(arrayFloat[arrayFloat.length-1-i]);
+            if (i < arrayThreeNum.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < words.length; i++) {
+            System.out.print(words[words.length-1-i]);
+            if (i < words.length - 1) {
+                System.out.print(", ");
+            }
         }
     }
 }
